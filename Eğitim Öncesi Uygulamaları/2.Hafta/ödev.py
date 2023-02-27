747be738d856563c80f5f6ece6ebab55d5afc777
Sara Nur Kay <saranurkay@gmail.com>
2023-02-27 19:19:58 +0300

    Create ödev.py

---
+++ Eğitim Öncesi Uygulamaları/2.Hafta/ödev.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Soru 1 
+# Yazılımcıların bir dili öğrenmeye başladığında ilk yapacakları şey ekrana "Hello World" yazdırmaktır. Python'da da aynı şeyi yapalım.Bir yazılımcı aşağıdaki kodu yazarken hata yapıyor. Bunu düzelterek çalışan bir kod yazalım.
+# 
+
+# In[ ]:
+
+
+print("Hello World")
+
+
+# ### Soru 2
+# İki YetGen'li zirvede karşılaşıyor. Zirvede birbirleriyle Python standında konuşurken YetGen Jump Python renginin ne olduğunu konuşurken bir YetGen lideri **logo** değişkenine atayıp ekrana bastırmalarını istiyor. Onlara bu noktada yardımcı olur musun?
+
+# In[ ]:
+
+
+logo = "Mor"
+print(logo)
+
+# ### Soru 3
+# Dairenin alanını hesaplayan bir Python kodu yazınız. pi sayısını 3.14 olarak alınız ve yarıçapını kullanıcıdan alınız. Sonucu ekrana bastırınız.
+
+# In[ ]:
+
+
+r=int(input("Yarı Çapı Gir: "))
+alan=3.14*r*r
+print("Alan: ",alan)
+
+
+
+# ### Soru 4
+# x ve y adında iki liste oluşturunuz. Bu listeleri birbiryle yer değiştirerek yazdırınız.
+# 
+# ```python
+# x = [1,2,3,4,5]
+# y = [5,4,3,2,1]
+# ``` 
+# 
+# ```python	
+# Output:
+# x = [5, 4, 3, 2, 1]
+# y = [1, 2, 3, 4, 5]
+# ```
+
+
+
+
+
+
+
+
+# ### Soru 5
+# Aşağıda verilen kod satırının sonuncunu 1 olacak şekilde düzenleyiniz.
+
+# In[ ]:
+
+
+5 - 3 // 2
+
+
+# ### Soru 6
+# Aşağıdaki kod satırının sonuncusunu 0 olacak şekilde düzenleyiniz.
+
+# In[ ]:
+
+
+8 - 3 * 2 - 1 + 1
+
+
+# ### Soru 7
+# YetGen liderlerinden Berkcan, Emirhan ve Eslem, Ramazan Bayramı'nda topladıkları şekerlerini bir araya getirmeyi ve kendi aralarında eşit olarak paylaşmayı kabul ettiler.
+# Eşit paylaşım yapıldıktan sonra kalan şekerler parçalanacak. Örneğin, eğer toplamda 91 şekerleri varsa , her biri 30 tane alıp kalan 1 tane şekeri parçalayacaklar.
+# Kaç tane şekeri parçalayacaklarını hesaplayan bir Python kodu yazınız.
+
+# In[ ]:
+
+
+# 7.Sorunun çözümünü buraya yazabilirsin.
+
